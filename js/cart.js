3a1ff88c82e9f617ff7242db427115e97c41ff75
Courtney Hans <cornta@yahoo.com>
2020-06-18 17:05:21 -0700

    DQCH built additional functions

--- js/cart.js
+++ js/cart.js
@@ -18,17 +18,38 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+// TODO: ***********Remove all of the rows (tr) in the cart table (tbody)
+function clearCart() {
+  var table = document.getElementById('cart');
+  var tableRows = document.getElementsByTagName('tbody');
+  for (var i = 0; i < cart.length; i++){
+  tableRows.deleteRow(0);
+}
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
-  // TODO: Find the table body
+  // TODO: *******Find the table body
+  var table = document.getElementById('cart');
+  var tableRows = document.getElementsByTagName('tbody');
 
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
+  // TODO: *********Iterate over the items in the cart
+  for (var i = 0; i < cart.length; i++){
+    var tableRow = document.createElement('tr');
+    var tableDataLink = document.createElement('td');
+    var images = CartItem.filePath
+//access cart item #1, pull out the filePath
+    var tableDataQuantity = document.createElement('td');
+    var itemPicture = 
+//access cart item #1, pull out quantity (check variable)
+    var tableDataItem = document.createElement('td');
+    var itemName = 
+//access cart item #2, pull out name
+  }
+
+  // TODO: ********Create a TR
+  // TODO: ********Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
@@ -36,8 +57,16 @@ function showCart() {
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  var deleteItem = document.getElementsById('will figure this out later');
+  deleteItem.addEventListener('Delete', cart.removeItem);
+
   // TODO: Save the cart back to local storage
+  var arrayOfItems = JSON.stringify(cart);
+  localStorage.clear();
+  localStorage.setItem('stored-cart-info', arrayOfItems);
+
   // TODO: Re-draw the cart table
+  showCart();
 
 }
 
